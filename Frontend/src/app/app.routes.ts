166a994930995a1aa174fde80bd9193c7614691c
Henrique <henriqueestrela2004@gmail.com>
2025-03-06 00:31:35 -0300

    up

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,27 +4,27 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
-
-    {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
-    },
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path:'',
-        component: LayoutComponent,
-
-        children: [
-            {
-                path:'dashboard',
-                component: DashboardComponent
-            }
-        ]
-    },
-    
-
+  // Rota inicial que redireciona para a página de login
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  // Rota para o Login
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  // Rota principal após o login
+  {
+    path: '',
+    component: LayoutComponent, // LayoutComponent será o "wrapper" para as rotas internas
+    children: [
+      {
+        path: 'dashboard', // Essa será a rota para o dashboard após o login
+        component: DashboardComponent
+      },
+      // Aqui você pode adicionar outras rotas filhas, se necessário
+    ]
+  }
 ];
